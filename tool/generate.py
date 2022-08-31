32d93ab5950f6f77b9dc029bf2d5ab079f4275ce
Hwlhwlxyz <huweilun1994@hotmail.com>
2022-08-30 23:25:29 -0400

    add function  that can change value in client-psutil-{uuid}-ws.py automatically to generate.py

--- tool/generate.py
+++ tool/generate.py
@@ -4,6 +4,8 @@ import uuid
 import secrets
 import os
 
+WEBSOCKETSERVER = "ws://localhost:8000/ws" # change to your server host
+
 def generate_config_server(generated_id, generated_password):
     return {
             "host":"input your host here",
@@ -14,6 +16,10 @@ def generate_config_server(generated_id, generated_password):
     }
 
 json_config = None
+client_py_name = None
+
+generated_id = str(uuid.uuid1())
+generated_password = secrets.token_urlsafe(15)
 
 if not os.path.exists("./config.json"):
     with open("./config.json", 'w') as file:
@@ -24,9 +30,8 @@ with open("./config.json", 'r') as file:
         json_config = {"servers":[]}
     else:
         json_config = json.load(file)
-    generated_id = str(uuid.uuid1())
-    generated_password = secrets.token_urlsafe(15)
-    shutil.copyfile('../client/client-psutil-ws.py', './client-psutil-ws'+generated_id+'.py')
+    client_py_name = './client-psutil-ws'+generated_id+'.py'
+    shutil.copyfile('../client/client-psutil-ws.py', client_py_name)
     
     server_cfg = generate_config_server(generated_id, generated_password)
     
@@ -38,3 +43,13 @@ with open("./config.json", 'w') as file:
         json_config['servers'].append(server_cfg)
     print(json_config)
     json.dump(json_config, file, indent=2)
+
+with open(client_py_name, 'r') as file:
+    client_py_data = file.read()
+client_py_data = client_py_data.replace('USER = "s01"', 'USER = "{}"'.format(generated_id))
+client_py_data = client_py_data.replace('PASSWORD = "some-hard-to-guess-copy-paste-password"', 'PASSWORD = "{}"'.format(generated_password))
+client_py_data = client_py_data.replace('WEBSOCKETSERVER = "ws://localhost:8000/ws"', 'WEBSOCKETSERVER = "{}"'.format(WEBSOCKETSERVER))
+
+with open(client_py_name, 'w') as file:
+    file.write(client_py_data)
+
